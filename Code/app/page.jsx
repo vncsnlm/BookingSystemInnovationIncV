65e9b2dcc798d30e950c5d80bc73af1d213a31d6
Saihaj Mann <saihajmann07@gmail.com>
2024-01-22 19:15:04 -0700

    sending the sample working app

---
+++ Code/app/page.jsx
@@ -0,0 +1,18 @@
+'use client';
+
+import React from 'react';
+
+import Hero from '../components/Hero';
+import Content from '../components/Content';
+
+export default function Index() {
+  return (
+    <>
+      <divhere>
+        <Hero />
+      </divhere>
+      <hr />
+      <Content />
+    </>
+  );
+}
