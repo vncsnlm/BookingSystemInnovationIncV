12135a16c705b43699f64031c3b4784a4423541c
Saihaj Mann <saihajmann07@gmail.com>
2024-03-09 12:17:26 -0700

    It passed and I don't know why
    also rebuild package-lock.json for some reason

--- Code/tests/example.spec.js
+++ Code/tests/example.spec.js
@@ -3,19 +3,6 @@ const { test, expect } = require('@playwright/test');
 
 //npx playwright test
 
-test('has title', async ({ page }) => {
-  await page.goto('https://localhost:3000/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Calendar App/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://localhost:3000/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('Exists', async ({ page }) => {
+  await page.goto('http://127.0.0.1:3000');
 });
