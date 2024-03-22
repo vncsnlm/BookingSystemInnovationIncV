9b7205faed5a5297933c1bbf9321805b3059a425
Saihaj Mann <saihajmann07@gmail.com>
2024-03-22 17:32:22 -0600

    checkpoint testing

--- Code/tests/admin.spec.js
+++ Code/tests/admin.spec.js
@@ -4,3 +4,9 @@ test('Test refresh button exists', async ({ page }) => {
     await page.goto('http://localhost:3000/admin');
     await page.getByRole('button', { name: 'Refresh' }).click();
   });
+
+test('Admin empty name', async ({ page }) => {
+    await page.goto('http://localhost:3000/admin');
+    await page.getByText('11:00 AM – 11:30 AMvictor1:30').click();
+    await expect(page.getByPlaceholder('Client name')).toHaveValue('');
+  });
--- Code/tests/adminTools.spec.js
+++ Code/tests/adminTools.spec.js
@@ -1,6 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
-test('Admin tool default name button', async ({ page }) => {
+//This test might be able to randomly fail for firefox
+test('Admin tool default name button full', async ({ page }) => {
     await page.goto('http://localhost:3000/admin');
     await page.getByText('11:00 AM – 11:30 AMvictor1:30').click();
     await page.getByRole('button').first().click();
@@ -24,6 +25,9 @@ test('Admin tool default name button works 1', async ({ page }) => {
     await expect(page.getByPlaceholder('Client name')).toHaveValue('Reserved');
   });
 
+//Work and work 2 can be deleted
+
+//Fail for all broswer, this test is potentially falty
 test('Admin tool default name button works 2', async ({ page }) => {
     await page.goto('http://localhost:3000/admin');
     await page.getByText('11:00 AM – 11:30 AMvictor1:30').click();
@@ -32,6 +36,7 @@ test('Admin tool default name button works 2', async ({ page }) => {
     await expect(page.getByPlaceholder('Client name')).toHaveValue('Reserved');
   });
 
+//Refer to works 1 test, the 3 line might be causing webkit to fail
 test('Admin tool default name button works', async ({ page }) => {
     await page.goto('http://localhost:3000/admin');
     await page.getByText('11:00 AM – 11:30 AMvictor1:30').click();
